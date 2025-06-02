430d632eadd2992302e6f363f7bce47d4c6141a7
David Aslan <davidaslan112671@live.se>
2025-06-02 14:52:59 +0200

    init

---
+++ src/test/java/hack_week/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hack_week.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
